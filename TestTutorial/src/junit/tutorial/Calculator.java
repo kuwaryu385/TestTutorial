e21105194df9e17e863a459f469f6d9c1c6be27d
Ryuta Kuwamura <staticjbulejass@gmail.com>
2021-01-09 15:47:50 +0900

    JUnit4のtTestチュートリアルの作成

---
+++ TestTutorial/src/junit/tutorial/Calculator.java
@@ -0,0 +1,16 @@
+package junit.tutorial;
+
+public class Calculator {
+
+	public int multiply(int x, int y) {
+		return x * y;
+	}
+
+	public float divide(int x, int y) {
+		if (y == 0)
+			throw new IllegalArgumentException("dicide by zero.");
+		return (float) x / (float) y;
+
+	}
+
+}
---
+++ TestTutorial/test/junit/tutorial/CalculatorTest.java
@@ -0,0 +1,19 @@
+package junit.tutorial;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void multiplyで3と4の乗算結果が取得できる() {
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
+	}
+
+}
